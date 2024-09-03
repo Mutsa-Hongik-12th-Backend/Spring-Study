a48a49bf635c4343fd20c1f98a46a2416327c03b
YUNA329 <ynmerry0329@gmail.com>
2024-09-03 18:28:29 +0900

    [Feat] Week1 과제 실습 프로젝트 생성

---
+++ week1/yunakang/src/main/java/hello/yunakang/YunakangApplication.java
@@ -0,0 +1,13 @@
+package hello.yunakang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunakangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunakangApplication.class, args);
+	}
+
+}
