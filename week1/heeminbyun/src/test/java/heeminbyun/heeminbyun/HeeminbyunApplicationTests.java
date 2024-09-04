e65679a9a6162d4e1db71a5717e19f64becc9f4a
byunhm02 <byunhm02@gmail.com>
2024-09-04 22:42:42 +0900

    [Feat]: Week1 프로젝트 환경설정

---
+++ week1/heeminbyun/src/test/java/heeminbyun/heeminbyun/HeeminbyunApplicationTests.java
@@ -0,0 +1,13 @@
+package heeminbyun.heeminbyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeeminbyunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
