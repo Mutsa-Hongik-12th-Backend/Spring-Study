c16adc79cefc67f66ad7923a2649308b9a6b5300
byunhm02 <byunhm02@gmail.com>
2024-09-05 16:49:00 +0900

    [Feat] Week1 과제 스프링 웹 개발 기초

--- week1/heeminbyun/src/main/java/heeminbyun/heeminbyun/controller/HelloController.java
+++ week1/heeminbyun/src/main/java/heeminbyun/heeminbyun/controller/HelloController.java
@@ -3,6 +3,9 @@ package heeminbyun.heeminbyun.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloController {
@@ -11,4 +14,37 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name",required=false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //api방식
+    @GetMapping("hello-string")
+    @ResponseBody //response body부에 "hello " + name 이 내용을 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        
+        public String getName() {
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+    }
+
+
 }
