05d989b883f8f542b839e42e42e3bd6a4c41f013
mdy3722 <mdy3722@gmail.com>
2024-09-09 23:31:51 +0900

    [Feat] week2 과제 제출

---
+++ week2/deokyoung/studyWeek2/src/main/java/mutsa/studyWeek2/controller/MemberController.java
@@ -0,0 +1,15 @@
+package mutsa.studyWeek2.controller;
+
+import mutsa.studyWeek2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
