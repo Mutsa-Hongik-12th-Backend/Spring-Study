59723e6c7029ef1d5f6b3bc6ab9412a7867a4d9a
fewolee <chulwoo25@naver.com>
2024-09-11 22:11:44 +0900

    [Feat] 회원 관리 API 구현

---
+++ week2/cheolwoo/demo/src/main/java/com/mutsa/cheolwoo/domain/Member.java
@@ -0,0 +1,12 @@
+package com.mutsa.cheolwoo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String name;
+}
---
+++ week2/cheolwoo/demo/src/main/java/com/mutsa/cheolwoo/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.mutsa.cheolwoo.service;
+
+import com.mutsa.cheolwoo.domain.Member;
+import com.mutsa.cheolwoo.repository.MemberRepository;
+import com.mutsa.cheolwoo.repository.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
