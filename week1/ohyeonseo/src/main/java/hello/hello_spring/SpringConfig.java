a4a9859742a611b990a6d4a6a5e2b48f8e921665
luckyyy <oyslucy@naver.com>
2024-09-12 17:32:07 +0900

    [feat] spring bean

---
+++ week1/ohyeonseo/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+        //생성자 주입
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
